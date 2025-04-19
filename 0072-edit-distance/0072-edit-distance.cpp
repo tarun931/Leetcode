e18595e9aa3960f348b6a6bd9d08d433135ea8e3
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-04-19 15:10:05 +1000

    Time: 0 ms (100%), Space: 10.6 MB (89.82%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -19,29 +19,53 @@ public:
     //     }    
     //     return min(one, min(two,min(three,four)));
     // }
+    // int helper(string word1, string word2)
+    // {
+    //     int n = word1.length();
+    //     int m = word2.length();
+    //     vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    //     for(int i=0;i<=n;i++)
+    //         dp[i][0] = i ;
+    //     for(int j=0; j<= m ;j++)
+    //         dp[0][j] = j ;    
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         for(int j = 1;j<=m ;j++)
+    //         {
+                 
+    //             if(word1[i-1]==word2[j-1])
+    //                 dp[i][j] = dp[i-1][j-1];
+    //             else
+    //             {
+    //                 dp[i][j] = 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+    //             }    
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
     int helper(string word1, string word2)
     {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)
-            dp[i][0] = i ;
+        //vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> prev(m+1,0) , cur(m+1,0);
         for(int j=0; j<= m ;j++)
-            dp[0][j] = j ;    
+            prev[j] = j ;    
         for(int i=1;i<=n;i++)
         {
+            cur[0] = i;     //needs to be updated // very imp.
             for(int j = 1;j<=m ;j++)
             {
-                 
                 if(word1[i-1]==word2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
                 else
                 {
-                    dp[i][j] = 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                    cur[j] = 1 + min(prev[j],min(cur[j-1],prev[j-1]));
                 }    
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
     int minDistance(string word1, string word2) {
         return helper(word1,word2);
