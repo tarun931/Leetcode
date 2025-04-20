c9e8f278f2b82f728ea265cda3e632937606ab1e
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-04-20 15:16:53 +1000

    Time: 0 ms (100%), Space: 12.1 MB (91.6%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -9,8 +9,19 @@ public:
         int cnt = 0 ;
         for(auto it : mp)
         {
-            cnt += ceil((double)it.second / (it.first+1)) * (it.first+1) ;
+            cnt += ceil((double)it.second / (it.first+1)) * (it.first+1) ; 
         }
         return cnt;
     }
 };
+
+/*
+    logic : ceil((double)it.second / (it.first+1)) * (it.first+1)
+
+    Divide the number of rabbits that gave answer x (i.e., it.second) by x + 1 — this gives us how many full groups we can form.
+
+But since we can't leave a group partially formed (e.g., if 5 rabbits say 2, but 3 rabbits fit in a group, we need 2 groups), we take the ceiling.
+
+Then, multiply by x + 1 to get the total rabbits accounted for (some might be theoretical rabbits not present in the array but implied by the answer).
+
+*/
