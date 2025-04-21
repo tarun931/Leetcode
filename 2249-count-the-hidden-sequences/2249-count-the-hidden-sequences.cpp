dde8db89de558bf15bdbc6620b81882b1bdf5754
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-04-21 11:32:35 +1000

    Time: 7 ms (21.77%), Space: 112.5 MB (75%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-       long long max_sum = INT_MIN;
-       long long min_sum=INT_MAX; 
+       long long max_sum = INT_MIN;   //its better to use 0 
+       long long min_sum=INT_MAX;     // its better to use 0
        long long sum = 0 ;
        for(int i=0;i<differences.size();i++)
        {
@@ -12,10 +12,27 @@ public:
         if(sum>max_sum)
              max_sum = sum ;           
        }
-        long long t1 = lower - min_sum ;
-        long long t2 = upper - max_sum ;
-       long long min_start = max(1LL*lower,t1) ;
-       long long max_start = min(1LL*upper,t2) ;
+       long long min_start = max(1LL*lower,lower - min_sum) ;    //had i used max_suma nd min_sum =0 then it would be easier // check the code below 
+       long long max_start = min(1LL*upper,upper - max_sum) ;
        return max(0LL, max_start-min_start+1);
     }
 };
+
+/*
+int numberOfArrays(vector<int>& differences, int lower, int upper) 
+    {
+        long long minSum = 0, maxSum = 0, current = 0;
+
+        for (int diff : differences) {
+            current += diff;
+            minSum = min(minSum, current);
+            maxSum = max(maxSum, current);
+        }
+
+        long long minStart = lower - minSum;
+        long long maxStart = upper - maxSum;
+
+        return max(0LL, maxStart - minStart + 1);
+        
+    }
+*/
