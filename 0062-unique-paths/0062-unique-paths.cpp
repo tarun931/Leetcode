e6145a703f0d3bab1e4f6b2e4fd802c9c851d229
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-04-21 11:55:16 +1000

    Time: 0 ms (100%), Space: 8.3 MB (74.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -16,7 +16,26 @@ public:
         }
         return dp[m-1][n-1] ;
     }
+    int helper2(int m , int n )
+    {
+        vector<int> prev(n,1),cur(n,0);
+        // for(int i=0;i<m;i++);
+        //     dp[i][0]=1;
+        // for(int i=0;i<n;i++)
+        //     dp[0][i] = 1;
+        for(int i=1;i<m;i++)
+        {
+            prev[0]=1;
+            cur[0]=1;
+            for(int j = 1;j<n;j++)
+            {
+                cur[j] = prev[j]+cur[j-1];    
+            }
+            prev=cur;
+        }
+        return prev[n-1];
+    }
     int uniquePaths(int m, int n) {
-        return helper(m,n);
+        return helper2(m,n);
     }
 };
