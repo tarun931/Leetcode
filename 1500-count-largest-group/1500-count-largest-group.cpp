75629b2f2972e02d1b7885bb4740859f20f3285d
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-04-23 11:57:19 +1000

    Time: 7 ms (6.1%), Space: 8.9 MB (11.53%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.cpp
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     int countLargestGroup(int n) {
-        unordered_map<int,int> mp;
-        int size = 0;
-        for(int i=1;i<=n;i++)
+        //unordered_map<int,int> mp;
+        int freq[37]={0};
+        int mf = 1;
+        int ans=1;
+        freq[1]=1;
+        for(int i=2;i<=n;i++)
         {
            string str = to_string(i);
            int digit =0 ;
            for(int j=0;j<str.length();j++)
            {
              digit = digit + (str[j] - '0');
-           } 
-           mp[digit] +=1 ;
-           size = max(size,mp[digit]);
-        }
-        int ans = 0;
-        for(auto it : mp)
-        {
-            if(it.second == size )
-                 ans++;
+           }
+            freq[digit] +=1 ;
+            if(freq[digit]==mf)
+               ans++;
+            else if(freq[digit]>mf)
+            {
+                mf = freq[digit];
+                ans = 1;
+            }   
         }
         return ans;
+        // for(int i=1;i<=n;i++)
+        // {
+        //    string str = to_string(i);
+        //    int digit =0 ;
+        //    for(int j=0;j<str.length();j++)
+        //    {
+        //      digit = digit + (str[j] - '0');
+        //    } 
+        //    mp[digit] +=1 ;
+        //    size = max(size,mp[digit]);
+        // }
+        // for(auto it : mp)
+        // {
+        //     if(it.second == size )
+        //          ans++;
+        // }
+        // return ans;
     }
 };
