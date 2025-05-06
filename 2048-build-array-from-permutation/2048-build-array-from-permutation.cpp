073b93ed6b24870686e5990b12d1c797b55f77d6
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-05-06 12:47:59 +1000

    Time: 2 ms (17.2%), Space: 20.5 MB (39.42%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
+    // vector<int> buildArray(vector<int>& nums) {
+    //     vector<int> ans;
+    //     for(int i=0;i<nums.size();i++)
+    //         ans.push_back(nums[nums[i]]);
+    //     return ans;
+    // }
+    //inplace 
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans;
+        //build array to store both original value(%1000) and final value(when divide by /1000) ,since values only range from [0,999]
         for(int i=0;i<nums.size();i++)
-            ans.push_back(nums[nums[i]]);
-        return ans;
+            nums[i] = nums[i] + 1000*(nums[nums[i]] % 1000); 
+        
+        // get values
+
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i] = nums[i]/1000 ;
+        }
+        return nums;
     }
 };
