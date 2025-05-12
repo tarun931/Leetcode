e09bbe0f238e2132137f18190a6235dca457fe0a
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-05-13 02:00:16 +1000

    Time: 191 ms (27.99%), Space: 13.8 MB (56.14%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isvalid(int num)
+    {
+        //cout<<num<<" ";
+        if(num/100 == 0)
+            return false;
+        if(num%2 != 0)
+            return false;
+        return true;       
+    }
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<digits.size();i++)
+        {
+            int one = digits[i];
+            //cout<<one<<" ";
+            if(one==0)
+                continue;
+            int two = -1;
+            int three = -1;
+            for(int j=0;j<digits.size();j++)
+            {
+               if(j==i)
+                  continue;
+                  
+               two = digits[j];
+               //cout<<two<<" ";
+               for(int k = 0;k<digits.size();k++)
+               {
+                if(k==i || k==j)
+                   continue;
+                three = digits[k];
+                //cout<<three<<" "<<k<<endl;
+                if(two == -1 || three == -1)
+                     continue;
+                  int n = one*100 + two*10 + three;
+                 if(isvalid(n) && mp.find(n)==mp.end()){
+                      ans.push_back(n);
+                      //cout<<n<<endl;
+                      mp[n]++;
+                 }    
+               }
+            }
+                   
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
