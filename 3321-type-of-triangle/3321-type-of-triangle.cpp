510e6d9b22b5a0eb72e3c4a5f8e0425650de686a
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-05-19 11:20:07 +1000

    Time: 0 ms (100%), Space: 23.1 MB (8.96%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-       unordered_map<int,int> mp;
-       int maxi=0;
-    //    int maxi1 = 0;
-    //    int maxi2= 0;
-    //    int maxi3=INT_MAX;
-       sort(nums.begin(),nums.end());
-       for(auto x : nums)
-       {
-        mp[x]++;
-        if(mp[x]>maxi)
-            maxi= mp[x];
-        // maxi1 = max(maxi1,x);
-        // maxi3 = min(maxi1,x);    
-       } 
-       if(nums[0]+nums[1]<=nums[2])
+        sort(nums.begin(), nums.end());
+        if (nums[0] + nums[1] <= nums[2])
             return "none";
-       if(maxi==3)
-           return "equilateral";
-       else if(maxi==2)
-           return "isosceles";    
-       return "scalene"     ;  
+        unordered_map<int, int> mp;
+        int maxi = 0;
+        for (auto x : nums) {
+            mp[x]++;
+            if (mp[x] > maxi)
+                maxi = mp[x];
+        }
+        if (maxi == 3)
+            return "equilateral";
+        else if (maxi == 2)
+            return "isosceles";
+        return "scalene";
     }
 };
