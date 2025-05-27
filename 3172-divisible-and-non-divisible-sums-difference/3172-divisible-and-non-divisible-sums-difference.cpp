d8c31f530aeeba7556a2c868a88517ab9a97d7eb
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-05-27 19:14:22 +1000

    Time: 0 ms (100%), Space: 8.5 MB (36.04%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        float t= (float)n*(((float)n+1.0)/2.0);
-        int sum = int(t); 
-        cout<<sum<<" ";
-        if(m==1)
-           return -sum;
-        int i = m ;
-        int s = 0;
-        while(i<=n)
-        {
-          s += i ;
-          i+=m;
-        }
-        int nd = sum-s;
-        cout<<nd<<" "<<s;
-        return nd-s;
+        int k = n / m;
+        return n * (n + 1) / 2 - k * (k + 1) * m;
     }
 };
