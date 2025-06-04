c081287ea9c47c313b240eeeac63c11aa156b8d5
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-06-04 19:21:42 +1000

    Time: 95 ms (16.79%), Space: 207.8 MB (6.72%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,19 +1,27 @@
-string lastSubstring(string s) {   //here we will extract lexicographicall largest string of any size
-        int i = 0, j = 1, n = s.size();
-        while (j < n) {
-            int k = 0;
-            while (j + k < n && s[i + k] == s[j + k]) {
-                k++;
-            }
-            if (j + k < n && s[i + k] < s[j + k]) {
-                i = j;
-                j = max(j + 1, j + k );
-            } else {
-                j = j + k + 1;
-            }
-        }
-        return s.substr(i, n - i);
-    }
+// string lastSubstring(string s) {   //here we will extract lexicographicall largest string of any size
+//         int i = 0, j = 1, n = s.size();
+//         while (j < n) {
+//             int k = 0;
+//             while (j + k < n && s[i + k] == s[j + k]) {
+//                 k++;
+//             }
+//             if (j + k < n && s[i + k] < s[j + k]) {
+//                 i = j;
+//                 j = max(j + 1, j + k );
+//             } else {
+//                 j = j + k + 1;
+//             }
+//         }
+//         return s.substr(i, n - i);
+//     }
+string lastSubstring(string str)
+{
+    string mx = "";
+    for (int i = 0; i < str.length(); ++i)
+        mx = max(mx, str.substr(i));
+
+    return mx;
+}
 class Solution {
 public:
     string answerString(string word, int numFriends) { 
