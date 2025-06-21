aec4dbde82a55d0ad9e030667e7c083b7dc757db
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-06-21 16:10:44 +1000

    Time: 87 ms (33.33%), Space: 18 MB (93.8%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,17 @@
+#defaultdict in python (collections). very much like unordered_map in c++
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        cnt = defaultdict(int)
+        for w in word:
+            cnt[w]+=1
+        ans = len(word)    
+        for a in cnt.values():
+            deleted = 0
+            for b in cnt.values():
+                if a>b:
+                    deleted += b;
+                elif b > (a+k):
+                    deleted += b-(a+k)
+            ans = min(ans,deleted)
+        return ans;                
