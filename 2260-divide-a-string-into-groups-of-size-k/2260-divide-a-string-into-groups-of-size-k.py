d432496f304214e94d41fc6327d4367c285c46f1
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-06-22 13:03:20 +1000

    Time: 0 ms (100%), Space: 17.8 MB (61.79%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.py
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.py
@@ -3,17 +3,9 @@ class Solution:
         ans = []
         size = len(s)
         ind = 0
-        # sol = ""
-        # for i in s:
-        #     if len(sol)==k:
-        #         ans.append(sol)
-        #         sol = ""
-        #     sol += i 
         while ind < size:
             ans.append(s[ind:ind+k])
             ind += k
-        # ans.append(sol)
         n = len(ans)
-        # m = len(ans[-1])
         ans[-1] += fill *( k-len(ans[-1]))
         return ans               
