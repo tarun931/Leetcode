c9320eacdbf38917771007ba94e4fad77142fa10
tarun931 <75002007+tarun931@users.noreply.github.com>
2025-06-22 12:51:08 +1000

    Time: 4 ms (2.44%), Space: 17.6 MB (98.65%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.py
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.py
@@ -1,26 +1,3 @@
-#  vector<string> ans ;
-#         string sol = "";
-#         for(int i=0;i<s.length();i++)
-#         {
-#             if(sol.size()==k)
-#             {
-#                 ans.push_back(sol);
-#                 sol.clear();
-#             }
-#             sol += s[i];
-#         }
-#         ans.push_back(sol);
-#         int n = ans.size();
-#         int m = ans[n-1].size();
-#         if(m<k)
-#         {
-#             while(m<k)
-#             {
-#                 ans[n-1] += fill ;
-#                 m = ans[n-1].size();
-#             }
-#         }
-#         return ans;
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
         ans = []
