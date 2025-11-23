354646a087927b774a67a4d4558f61e6364e0e41
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2025-11-23 23:19:10 +1100

    Time: 29 ms (63.31%), Space: 21.9 MB (45.27%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -20,9 +20,14 @@ class Solution:
         return ans + sum(a)                 
 
         
+'''
+in this question, out approach is :
+make 3 arrays : a, b ,c made up of ele which give remainders 0,1,2 when divided by 3.
+we can use all ele in a , since all the remainder will always. be 0
+we can use a combo of elements in b and c  (1*cntb + 2*cntc)mod 3 , (one ele from b will give 1 remainder. ,lets say we choose cntb ele from b that means they will contri cntb*1 in remainder , and 2 elements(cntc=2) from c will give 2 remainders each ) .  We need to find a combo such that (1*cntb + 2*cntc) mod 3 == 0 . and also the largest sum. 
+Now in b. ,there are only 3 remainders possible : sum(b) , sum(b-1) and sum(b-2) .  because , if we do sum(b-3)mod 3 == sum(b)mod 3 only , so to maximize sum ,only sum(b) , sum(b-1) and sum(b-2) will be considered .  Similarly in c , only sum(c) , sum(c-1) and sum(c-2)  are considered , because , only these 3 types of remainders are possible , either  0 ,2 , 4  , in case of sum(c-3)%3 will be same as sum(c)%3 only ,becuase (2 +2+2 )% 3 == 0 only. 
+So in total we will try these 9 combo and get the best sum possible and add to sum(a). 
+and that will be our final answer.  
 
 
-
-
-                        
-        
+'''
