90a68b3b8408b8b52dcb76da1229fd2a70175ecf
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2025-11-25 22:13:01 +1100

    Time: 3 ms (97.49%), Space: 17.8 MB (56.41%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -2,6 +2,8 @@ class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
         if k%2==0 or k%10 == 5 :
             return -1
+        if k==1 :
+            return 1    
         rem = 0
         for l in range(1,k+1): # if I am divinding a number by 3 , only possible remainders should be 0,1,2,. If we get 0 , we get our answer , but if we get 1,2 and then again if we get 1 or 2 , that means , we will never get 0 as the rem , it will forever stay in 1 and 2 . 
             rem = (rem*10+1)%k
