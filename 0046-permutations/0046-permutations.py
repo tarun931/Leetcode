43d630a1f7f2bda875990c577182db3c5754bd78
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2025-11-27 20:50:47 +1100

    Time: 1 ms (47.1%), Space: 18.2 MB (16.34%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = []
+        def helper(start):
+            if start>= n :
+                ans.append(nums[:])
+                return
+            for i in range(start , len(nums)):
+                nums[start], nums[i] = nums[i] , nums[start]
+                helper(start+1)
+                nums[start], nums[i] = nums[i] , nums[start]
+            
+        helper(0)       
+        return ans    
+
+
+
+        
