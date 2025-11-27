fbf13e78d1032e7751529cecc97e56df7049b402
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2025-11-27 20:52:57 +1100

    Time: 0 ms (100%), Space: 18 MB (34.06%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,21 @@
+from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        ans = []
-        def helper(start):
-            if start>= n :
-                ans.append(nums[:])
-                return
-            for i in range(start , len(nums)):
-                nums[start], nums[i] = nums[i] , nums[start]
-                helper(start+1)
-                nums[start], nums[i] = nums[i] , nums[start]
+        # n = len(nums)
+        # ans = []
+        # def helper(start):
+        #     if start>= n :
+        #         ans.append(nums[:])
+        #         return
+        #     for i in range(start , len(nums)):
+        #         nums[start], nums[i] = nums[i] , nums[start]
+        #         helper(start+1)
+        #         nums[start], nums[i] = nums[i] , nums[start]
             
-        helper(0)       
-        return ans    
+        # helper(0)       
+        # return ans
+        all_perms = sorted(set(permutations(nums)))
+        return all_perms    
 
 
 
