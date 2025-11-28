2b8cd64e692b9584325b858ea5ee896b51ceebf8
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2025-11-29 05:15:26 +1100

    Time: 124 ms (55.8%), Space: 36.9 MB (76.09%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -1,49 +1,5 @@
 class Solution:
-    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        # g = [[] for _ in range(n)]
-        # for u, v in edges:
-        #     g[u].append(v)
-        #     g[v].append(u)
-
-        # # Pick root = 0 (you tried to find real root, but in a tree ANY root works)
-        # root = 0
-
-        # # DFS: returns (components_in_subtree, remainder_of_subtree)
-        # def dfs(node, parent):
-        #     total_components = 0
-        #     remainder_sum = values[node] % k
-
-        #     for child in g[node]:
-        #         if child == parent:
-        #             continue
-
-        #         child_components, child_remainder = dfs(child, node)
-
-        #         total_components += child_components
-
-        #         if child_remainder == 0:
-        #             # child subtree is divisible → CUT IT
-        #             total_components += 1
-        #         else:
-        #             # must merge child's remainder into parent
-        #             remainder_sum = (remainder_sum + child_remainder) % k
-
-        #     # If after merging all children, the parent's sum is divisible:
-        #     # The entire subtree becomes a new component
-        #     if remainder_sum == 0:
-        #         return total_components, 0
-
-        #     return total_components, remainder_sum
-
-        # components, rem = dfs(root, -1)
-
-        # # If root also becomes divisible, count it
-        # if rem == 0:
-        #     components += 1
-
-        # return components
-
-        # make adjacency list 
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int: 
         g = [[] for _ in range(n)]
         for u , v in edges:
             g[u].append(v)
