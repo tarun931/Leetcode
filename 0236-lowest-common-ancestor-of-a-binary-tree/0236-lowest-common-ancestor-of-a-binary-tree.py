a68118c3d43d15ba841b1746eb4122af0ae80e76
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2025-12-05 09:42:49 +1100

    Time: 55 ms (49.14%), Space: 22.1 MB (43.41%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,20 +6,32 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        ans  = None
-        def anc(node , p,q):
-            nonlocal ans
-            if not node:
-                return False , False
-            lftp , lftq = anc(node.left,p,q)
-            rtp , rtq = anc(node.right,p,q)   
-            cp  = lftp or rtp or node==p 
-            cq  = lftq or rtq or node==q
-            if cp and cq and ans == None :
-                ans = node
-            return cp,cq
-        anc(root , p ,q)
-        return ans        
+    # def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    #     ans  = None
+    #     def anc(node , p,q):
+    #         nonlocal ans
+    #         if not node:
+    #             return False , False
+    #         lftp , lftq = anc(node.left,p,q)
+    #         rtp , rtq = anc(node.right,p,q)   
+    #         cp  = lftp or rtp or node==p 
+    #         cq  = lftq or rtq or node==q
+    #         if cp and cq and ans == None :
+    #             ans = node
+    #         return cp,cq
+    #     anc(root , p ,q)
+    #     return ans  
+
+    def lowestCommonAncestor(self, root, p, q):
+        if not root :
+            return None
+        if root == p or root == q:
+            return root
+        lc = self.lowestCommonAncestor( root.left , p , q )
+        rc = self.lowestCommonAncestor(root.right , p , q)
+        if lc and rc :
+            return root
+        return lc if lc else rc    
+
 
         
