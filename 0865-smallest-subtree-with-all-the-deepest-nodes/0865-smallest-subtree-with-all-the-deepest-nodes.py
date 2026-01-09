cad99ce184aca58e30c3a28a1f7476791f9ebb64
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2026-01-10 04:10:55 +1100

    Time: 0 ms (100%), Space: 19.6 MB (11.5%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -5,45 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # self.parent = {} 
-        # self.ans = []
-        # self.parent[root] = None
-        # def helper(root):
-        #     q = deque()
-        #     q.append(root)
-        #     while q:
-        #         l = len(q)
-        #         res = []
-        #         for i in range(l):
-        #             cur = q.popleft()
-        #             if cur :
-        #                 if cur.left:
-        #                 q.append(cur.left)
-        #                 self.parent[cur.left] = cur
-        #                 if cur.right:
-        #                     q.append(cur.right)
-        #                     self.parent[cur.right] = cur
-        #             res.append(cur)
-        #         self.ans.append(res)
-        #     return 
-        # helper(root)
-        # res = self.ans.pop()
-        # sol = set()
-        # l = len(res)
-        # if res[0] == root:
-        #     return root
-        # i = 0    
-        # self.p = set()
-        # while len(self.p)>1 or i==0 :
-        #     i+=1
-        #     if i != 0:
-        #         res[:] = self.p
-        #     self.p.clear()
-        #     for ele in res:
-        #         self.p = self.parent[ele]
-
-        # return self.p.pop()    
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:    
         parent = {root: None}
 
         # BFS
