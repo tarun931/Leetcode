fcd126bdb7296dd4028d385bfd9f993b9a997597
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2026-01-10 04:20:13 +1100

    Time: 0 ms (100%), Space: 19.4 MB (11.5%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -4,39 +4,57 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:    
+#         parent = {root: None}
+
+#         # BFS
+#         q = deque([root])
+#         last_level = []
+
+#         while q:
+#             level = []
+#             for _ in range(len(q)):
+#                 node = q.popleft()
+#                 level.append(node)
+
+#                 if node.left:
+#                     parent[node.left] = node
+#                     q.append(node.left)
+#                 if node.right:
+#                     parent[node.right] = node
+#                     q.append(node.right)
+
+#             last_level = level  
+
+#         # Start from deepest nodes
+#         curr = set(last_level)
+
+#         # Climb parents until one node remains
+#         while len(curr) > 1:
+#             curr = set(parent[node] for node in curr)
+
+#         return curr.pop()
+   
 class Solution:
     def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:    
-        parent = {root: None}
+        def dfs(node):
+            if not node:
+                return (0, None)
+            ld, lnode = dfs(node.left)
+            rd, rnode = dfs(node.right)
 
-        # BFS
-        q = deque([root])
-        last_level = []
+            if ld > rd:
+                return (ld + 1, lnode)
+            if rd > ld:
+                return (rd + 1, rnode)
 
-        while q:
-            level = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                level.append(node)
+            return (ld + 1, node)
 
-                if node.left:
-                    parent[node.left] = node
-                    q.append(node.left)
-                if node.right:
-                    parent[node.right] = node
-                    q.append(node.right)
+        return dfs(root)[1]
 
-            last_level = level  
-
-        # Start from deepest nodes
-        curr = set(last_level)
-
-        # Climb parents until one node remains
-        while len(curr) > 1:
-            curr = set(parent[node] for node in curr)
-
-        return curr.pop()
    
-            
+                        
     
 
                 
