a1e074bfa7b10912be6f0ba3f0d53fd56f0eb0e3
Tarun Singh <75002007+tarun931@users.noreply.github.com>
2026-02-13 18:47:58 +1100

    Time: 11 ms (25.43%), Space: 23 MB (42.54%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d ={}
+        for i in range(0, len(nums)):
+            if nums[i] not in d :
+                d[nums[i]] = 1
+            else :
+                d[nums[i]] += 1        
+        
+        maxHeap = [(-freq , ele) for ele , freq in d.items()]
+
+        heapq.heapify(maxHeap)
+
+        ans = []
+
+        for i in range(k , 0 ,-1):
+            _ , ele = heapq.heappop(maxHeap)
+            print(ele)
+            ans.append(ele)    
+        return ans    
+
+
+
+        
